1d820da37cc2fb4abac60d70c3ac22ed507e267f
Gaini Laxman <gainilaxman12@gmail.com>
2023-05-13 00:46:36 +0530

    loadbalancer and ribbon and round robin technic added

--- src/main/java/com/klinnovations/rest/GreetMsgController.java
+++ src/main/java/com/klinnovations/rest/GreetMsgController.java
@@ -1,16 +1,24 @@
 package com.klinnovations.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetMsgController {
 
-	
-	@GetMapping("/greet")
-	public String  getGreetMsg() {
+	@Autowired
+	private Environment env;
 
-		return "Good Evening";
+	@GetMapping("/greet")
+	public String getGreetMsg() {
+
+		String port = env.getProperty("server.port");
+
+		return "Good Evening(" + port + ")";
 	}
 
+	//-Dserver.port=8081
+	
 }
